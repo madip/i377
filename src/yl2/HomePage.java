2d5cfab216e92570fba51a09230b8d2c6b906c49
madip <madispeep@gmail.com>
2013-09-29 21:58:53 +0300

    Rollback

--- src/yl2/HomePage.java
+++ src/yl2/HomePage.java
@@ -8,22 +8,21 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-
 public class HomePage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		String id = session.getId();
+
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+		HttpSession sess = request.getSession();
+		String id = sess.getId();
 
 		Object attr = request.getParameter("param");
-		session.setAttribute("param", attr);
-		String sessionAttr = (String) session.getAttribute("param");
+		sess.setAttribute("param", attr);
+		String sessAttr = (String) sess.getAttribute("param");
 
 		response.getWriter().println("Hello");
-		response.getWriter().println("session ID: " + id);
-		response.getWriter().println("session attribute is " + sessionAttr);
+		response.getWriter().println("Session ID: " + id);
+		response.getWriter().println("Session attribute is " + sessAttr);
 	}
-}
 
+}
