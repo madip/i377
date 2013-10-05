7ec82496c1d201afdce6082ac8680c468955f911
madip <madispeep@gmail.com>
2013-10-05 18:41:16 +0300

    logout test

--- src/yl2/LogOut.java
+++ src/yl2/LogOut.java
@@ -1,10 +1,12 @@
 package yl2;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class LogOut extends HttpServlet {
@@ -12,8 +14,12 @@ public class LogOut extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//logout
-		request.getSession().invalidate();
+		HttpSession sess = request.getSession();
+		sess.invalidate();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 	}
 
 }
