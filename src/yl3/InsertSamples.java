013725a02e43d1f307f9581f3dc7b70e4ace75de
madip <madispeep@gmail.com>
2013-10-13 17:40:28 +0300

    test insert data test

--- src/yl3/InsertSamples.java
+++ src/yl3/InsertSamples.java
@@ -5,9 +5,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import db.SetupDao;
 
-public class SampleData extends HttpServlet {
+public class InsertSamples extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
